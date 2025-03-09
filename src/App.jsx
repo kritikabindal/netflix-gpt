040ab50667e1c350b6026baf72ef5eae5920e3bf
Kritika Bindal <kritikabindal30@gmail.com>
2025-03-09 14:20:49 +0530

    first_commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <div className="text-3xl font-bold text-green-800">Namaste Everyone, Welcome to Netflix GPT!</div>
+    </>
+  )
+}
+
+export default App
